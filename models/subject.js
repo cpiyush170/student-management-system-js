28134e2692b7f087240cb156946990040ed54211
Piyush <cpiyush170@gmail.com>
2023-02-12 16:18:28 +0530

    fixes

--- models/subject.js
+++ models/subject.js
@@ -1,10 +1,24 @@
 const mongoose = require('mongoose');
+const Teacher = require('./teacher')
+
 const Schema = mongoose.Schema
 
 const subjectSchema = new Schema({
   name: {
     type: String,
     required: true,
+  }
+})
+
+
+subjectSchema.pre('remove', async function (next) {
+
+  let teachers = await Teacher.find({ subject: this.Subject.id })
+  if (teachers.length > 0) {
+    next(new Error('Cannot delete subject with teachers'))
+  }
+  else {
+    next();
   }
 })
 
